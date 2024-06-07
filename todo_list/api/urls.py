ef27341f8cb6e83aaf2e44a1f1b88dbba4adf97d
vimansha <vimanshamadushani88@gmail.com>
2024-06-07 18:25:36 +0530

    feat: task_detail.html

--- todo_list/api/urls.py
+++ todo_list/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskDetail, TaskList, DetailView
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasks')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task')
+
 ]
