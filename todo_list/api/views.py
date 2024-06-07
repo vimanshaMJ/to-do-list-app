5b84a31243e534301fa142f9920b96f6ef73641a
vimansha <vimanshamadushani88@gmail.com>
2024-06-07 19:09:39 +0530

    feat: create, update and delete task views

--- todo_list/api/views.py
+++ todo_list/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Task
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -12,3 +14,18 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task  
     context_object_name = 'task'   # for task_detail
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks') 
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task' 
+    success_url = reverse_lazy('tasks')
