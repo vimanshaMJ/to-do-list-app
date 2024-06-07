a3094559cdf43e75adadd51e818a1dec34fd2cbb
vimansha <vimanshamadushani88@gmail.com>
2024-06-07 22:14:47 +0530

    feat: restricted user

--- todo_list/api/views.py
+++ todo_list/api/views.py
@@ -1,4 +1,5 @@
 from dataclasses import fields
+from multiprocessing import context
 from pyexpat import model
 from django.shortcuts import render
 from django.views.generic.list import ListView
@@ -16,6 +17,12 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'  # for task_list
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user = self.request.user)
+        context['count'] = context['tasks'].filter(complete = False).count()
+        return context
+    
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task  
@@ -23,12 +30,17 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks') 
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+    
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 class TaskDelete(LoginRequiredMixin, DeleteView):
